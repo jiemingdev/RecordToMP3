7e6c1d20cee251f2ff0b2521ee9b45133b421689
jieming123 <zhouxin@zhouxindeMacBook-Pro.local>
2017-09-19 09:57:45 +0800

    Initial Commit

---
+++ RecordToMP3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecordToMP3
+//
+//  Created by 周鑫 on 2017/9/19.
+//  Copyright © 2017年 周鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
